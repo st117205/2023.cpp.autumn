d82084ce8abcc28c5e42efd8cc7263caf2fad396
st117205 <144107148+st117205@users.noreply.github.com>
2023-09-15 19:31:58 +0300

    Create task7.cpp

---
+++ 2023.cpp.autumn.homework/2023.09.15.homework1/task7.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+
+int main() {
+	int n;
+	std::cin >> n;
+	std::cout << "The next number for the number " << n << " is " << n + 1 << "." << std::endl;
+	std::cout << "The previous number for the number " << n << " is " << n - 1 << ".";
+	return 0;
+}
