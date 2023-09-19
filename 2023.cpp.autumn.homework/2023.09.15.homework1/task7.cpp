0a08021a9377e85b6a35126fa874aeeb25a73898
st117205 <144107148+st117205@users.noreply.github.com>
2023-09-19 18:55:55 +0300

    Update task7.cpp

--- 2023.cpp.autumn.homework/2023.09.15.homework1/task7.cpp
+++ 2023.cpp.autumn.homework/2023.09.15.homework1/task7.cpp
@@ -1,9 +1,9 @@
 ﻿#include <iostream>
 
-int main() {
-	int n;
+int main(int argc, char* argv[]) {
+	int n = 0;
 	std::cin >> n;
 	std::cout << "The next number for the number " << n << " is " << n + 1 << "." << std::endl;
 	std::cout << "The previous number for the number " << n << " is " << n - 1 << ".";
-	return 0;
+	return EXIT_SUCCESS;
 }
