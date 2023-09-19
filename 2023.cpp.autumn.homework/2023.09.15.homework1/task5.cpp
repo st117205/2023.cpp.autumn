7a08ee2f342cf1c8b04d4257ab70396106fe98c9
st117205 <144107148+st117205@users.noreply.github.com>
2023-09-19 18:55:41 +0300

    Update task5.cpp

--- 2023.cpp.autumn.homework/2023.09.15.homework1/task5.cpp
+++ 2023.cpp.autumn.homework/2023.09.15.homework1/task5.cpp
@@ -1,12 +1,10 @@
 ﻿#include <iostream>
 
-int main() {
-	int v;
+int main(int argc, char* argv[]) {
+	int v = 0;
+	int t = 0;
 	std::cin >> v;
-	int t;
 	std::cin >> t
 	std::cout << ((v * t) % 109 + 109 * 10) % 109 + 1;
-	return 0;
-
-
+	return EXIT_SUCCESS;
 }
