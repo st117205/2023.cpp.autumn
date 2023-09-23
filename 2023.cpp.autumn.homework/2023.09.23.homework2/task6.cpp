d1be81d80b9a3a6592db8a58494845775f345598
st117205 <144107148+st117205@users.noreply.github.com>
2023-09-23 17:15:43 +0300

    Create task6.cpp

---
+++ 2023.cpp.autumn.homework/2023.09.23.homework2/task6.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int main(int atgc, char* argv[]) {
+	int k = 0;
+	int m = 0;
+	int n = 0;
+	int p = 0;
+	std::cin >> k;
+	std::cin >> m;
+	std::cin >> n;
+	if (n % k == 0) {
+		p = n / k;
+	}
+	else {
+		p = n / k + 1;
+	}
+	std::cout << 2 * m * p;
+	return EXIT_SUCCESS;
+}
