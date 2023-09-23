c3f01e8d6e0fdb823d5a4d0c9f312425bc5fa102
st117205 <144107148+st117205@users.noreply.github.com>
2023-09-23 17:15:20 +0300

    Create task1.cpp

---
+++ 2023.cpp.autumn.homework/2023.09.23.homework2/task1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+int main(int argc, char* argv[]) {
+	int n = 0;
+	std::cin >> n;
+	if ((n % 4 == 0 and n % 100 != 0) or n % 400 == 0) {
+		std::cout << "YES";
+	}
+	else {
+		std::cout << "NO";
+	}
+	return EXIT_SUCCESS;
+}
