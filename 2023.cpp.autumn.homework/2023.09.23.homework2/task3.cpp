a7ea71d9dec3d4c35425c615d32043ab518531af
st117205 <144107148+st117205@users.noreply.github.com>
2023-09-23 17:15:29 +0300

    Create task3.cpp

---
+++ 2023.cpp.autumn.homework/2023.09.23.homework2/task3.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <math.h>
+
+int main(int argc, char* argv[]) {
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	std::cin >> a;
+	std::cin >> b;
+	std::cin >> c;
+	std::cin >> d;
+	if (a == 0 and b == 0) {
+		std::cout << "INF";
+	}
+	else if ((a != 0 and b != 0 and abs(b) % abs(a) == 0 and abs(d) % abs(c) == 0 and -b / a != -d / c) or (a != 0 and b != 0 and abs(b) % abs(a) == 0 and abs(d) % abs(c) != 0) or (a != 0 and b == 0)) {
+		std::cout << -b / a;
+	}
+	else {
+		std::cout << "NO";
+	}
+	return EXIT_SUCCESS;
+}
