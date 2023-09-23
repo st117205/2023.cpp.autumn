fa45b2f2cd7349f12f4a8f31bf8a7195590c1ed8
st117205 <144107148+st117205@users.noreply.github.com>
2023-09-23 17:15:39 +0300

    Create task5.cpp

---
+++ 2023.cpp.autumn.homework/2023.09.23.homework2/task5.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main(int argc, char* argv[]) {
+	int n = 0;
+	std::cin >> n;
+		if ((n % 100) > 10 and (n % 100 < 20)) {
+			std::cout << n << " bochek";
+		}
+		else {
+			if (n % 10 == 1) {
+				std::cout << n << " bochka";
+			}
+			if (n % 10 == 2 or n % 10 == 3 or n % 10 == 4) {
+				std::cout << n << " bochki";
+			}
+			if (n % 10 == 0 or n % 10 == 5 or n % 10 == 6 or n % 10 == 7 or n % 10 == 8 or n % 10 == 9) {
+				std::cout << n << " bochek";
+			}
+		}
+	return EXIT_SUCCESS;
+} 
