f1dc4390015a93e022ec66dc5a09583ea1a51268
st117205 <144107148+st117205@users.noreply.github.com>
2023-10-01 03:28:00 +0300

    Create task9.cpp

---
+++ 2023.cpp.autumn.homework/2023.01.10.homework3/task9.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+	int a = 0;
+	int b = 0;
+	std::cin >> a;
+	std::cin >> b;
+	while (a > b) {
+		if (a % 2 == 0 and a / b > 1) {
+			a = a / 2;
+			std::cout << ":2" << std::endl;
+		}
+		else {
+			a -= 1;
+			std::cout << "-1" << std::endl;
+		}
+	}
+	return EXIT_SUCCESS;
+}
