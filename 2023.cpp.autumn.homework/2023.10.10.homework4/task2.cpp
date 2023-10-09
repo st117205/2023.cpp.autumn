b40cd7f3cf8e68290d3569de10084745e426dc63
st117205 <144107148+st117205@users.noreply.github.com>
2023-10-10 01:23:52 +0300

    Create task2.cpp

---
+++ 2023.cpp.autumn.homework/2023.10.10.homework4/task2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+
+int main(int, char**) {
+	int n = 0;
+	int c = 0;
+	std::cin >> n;
+	int* a = (int*)malloc(sizeof(int*) * n);
+	for (int i = 0; i < n; ++i) {
+		std::cin >> c;
+		*(a + i) = c;
+	}
+	for (int i = 0; i < n; i + 2) {
+		std::cout << *(a + i) << " ";
+	}
+	free(a);
+	return EXIT_SUCCESS;
+}
