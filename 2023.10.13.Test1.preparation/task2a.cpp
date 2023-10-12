0ba55a04e89c1705f5a5cc983c6bdd6253c6d8b8
st117205 <144107148+st117205@users.noreply.github.com>
2023-10-12 15:28:55 +0300

    Create task2a.cpp

---
+++ 2023.10.13.Test1.preparation/task2a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(int, char**)
+{
+	int n = 0;
+	std::cin >> n;
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			std::cout << j + 1 << " ";
+		}
+		std::cout << std::endl;
+	}
+	return EXIT_SUCCESS;
+}
