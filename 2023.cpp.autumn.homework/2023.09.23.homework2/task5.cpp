56b050ad2f1e40a219bb89b26b830a1ebc7dbe90
st117205 <144107148+st117205@users.noreply.github.com>
2023-11-15 12:10:15 +0300

    Update task5.cpp

--- 2023.cpp.autumn.homework/2023.09.23.homework2/task5.cpp
+++ 2023.cpp.autumn.homework/2023.09.23.homework2/task5.cpp
@@ -3,17 +3,17 @@
 int main(int argc, char* argv[]) {
 	int n = 0;
 	std::cin >> n;
-		if ((n % 100) > 10 and (n % 100 < 20)) {
+		if ((n % 100) > 10 && (n % 100 < 20)) {
 			std::cout << n << " bochek";
 		}
 		else {
 			if (n % 10 == 1) {
 				std::cout << n << " bochka";
 			}
-			if (n % 10 == 2 or n % 10 == 3 or n % 10 == 4) {
+			if (n % 10 == 2 || n % 10 == 3 || n % 10 == 4) {
 				std::cout << n << " bochki";
 			}
-			if (n % 10 == 0 or n % 10 == 5 or n % 10 == 6 or n % 10 == 7 or n % 10 == 8 or n % 10 == 9) {
+			if (n % 10 == 0 || n % 10 == 5 || n % 10 == 6 || n % 10 == 7 || n % 10 == 8 || n % 10 == 9) {
 				std::cout << n << " bochek";
 			}
 		}
