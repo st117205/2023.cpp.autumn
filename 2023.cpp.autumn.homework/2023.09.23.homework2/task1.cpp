0dc2d4d377d2808a551cf02b692ec108faa489a9
st117205 <144107148+st117205@users.noreply.github.com>
2023-11-15 12:09:59 +0300

    Update task1.cpp

--- 2023.cpp.autumn.homework/2023.09.23.homework2/task1.cpp
+++ 2023.cpp.autumn.homework/2023.09.23.homework2/task1.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char* argv[]) {
 	int n = 0;
 	std::cin >> n;
-	if ((n % 4 == 0 and n % 100 != 0) or n % 400 == 0) {
+	if ((n % 4 == 0 && n % 100 != 0) || n % 400 == 0) {
 		std::cout << "YES";
 	}
 	else {
